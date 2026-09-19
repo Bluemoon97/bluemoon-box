cb2468a52f1499a5d123b0dfcde4beee939712df
Bluemoon97 <porter1max@yahoo.co.jp>
2026-09-20 00:11:20 +0900

    Add files via upload

---
+++ cocartly/js/barcode/code.js
@@ -0,0 +1,37 @@
+/* ==========================================
+   コード読取
+   ========================================== */
+
+function readCode() {
+
+    if (
+        typeof updateCodeScreenLanguage ===
+        "function"
+    ) {
+
+        updateCodeScreenLanguage();
+
+    }
+
+    startBarcodeReader();
+
+}
+
+/*
+ バーコード読取の使用目的
+
+ normal
+ → 通常の商品検索
+
+ priceRecordJan
+ → 今回の購入の商品コード入力
+
+ shoppingAddJan
+ → 購入予定＋追加の商品コード入力
+
+ outingAddJan
+ → お出かけ＋追加の商品選択
+*/
+
+let barcodeReadMode =
+    "normal";
